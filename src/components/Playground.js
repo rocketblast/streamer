819d83eeb3e0d58940b10c3d661babf937b0c360
Martin Danielson <martin@rocketblast.com>
2018-04-07 02:52:00 +0200

    Layout tweaks

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -4,18 +4,23 @@ import React from 'react';
 import TwitchPlayer from './Twitch/Player';
 
 const Playground = () => {
-    const spacing = 50;
-
     return (
-        <div style={ { display: 'flex' } }>
-            <div style={ { boxSizing: 'border-box', background: '', width: `calc(75.3% - ${ (spacing / 2).toFixed(2) }px)`, paddingLeft: spacing } }>
+        <div style={ { display: 'flex', paddingTop: 100 } }>
+            <div style={ { boxSizing: 'border-box', background: '', width: '73%', paddingLeft: '6%' } }>
                 <TwitchPlayer channel="monstercat" />
             </div>
-            <div style={ { boxSizing: 'border-box', background: '', width: `calc(24.7% - ${ (spacing / 2).toFixed(2) }px)`, paddingLeft: spacing } }>
+            <div style={ {
+                boxSizing: 'border-box',
+                background: '',
+                width: '27%',
+                paddingLeft: '2%',
+                paddingRight: '6%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+            } }>
                 <TwitchPlayer channel="rocketblasttv" importance="less" />
-                <div style={ { height: spacing, content: ' ' } } />
                 <TwitchPlayer channel="wizzlertv" importance="less" />
-                <div style={ { height: spacing, content: ' ' } } />
                 <TwitchPlayer channel="anything4views" importance="less" />
             </div>
             {/*<TwitchChat />*/}
