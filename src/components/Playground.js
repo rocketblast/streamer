8666b8b48ee517252e478ce07ad2af5e7d37f7e6
Martin Danielson <martin@rocketblast.com>
2018-04-07 19:51:25 +0200

    API token, User API and some design tweaks

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import AuthorizationContext from './Twitch/AuthorizationContext.js';
 // import TwitchChat from './Twitch/IFrameChat';
 import TwitchPlayer from './Twitch/Player';
 
-const Playground = () => {
-    return (
-        <div style={ { display: 'flex', paddingTop: 100 } }>
-            <div style={ { boxSizing: 'border-box', background: '', width: '73%', paddingLeft: '6%' } }>
-                <TwitchPlayer channel="monstercat" />
-            </div>
-            <div style={ {
-                boxSizing: 'border-box',
-                background: '',
-                width: '27%',
-                paddingLeft: '2%',
-                paddingRight: '6%',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-            } }>
-                <TwitchPlayer channel="rocketblasttv" importance="less" />
-                <TwitchPlayer channel="wizzlertv" importance="less" />
-                <TwitchPlayer channel="anything4views" importance="less" />
-            </div>
-            {/*<TwitchChat />*/}
-        </div>
-    );
+class Playground extends Component {
+    componentDidMount() {
+        fetch(`https://id.twitch.tv/oauth2/token?client_id=mh7ueh4ek1qokl273t606xkeob7f1xo&client_secret=${ process.env.REACT_APP_TWITCH_SECRET }&grant_type=client_credentials`, {
+            method: 'POST'
+        })
+            .then(response => response.json())
+            .then(data => this.setState(state => ({
+                ...data,
+            })));
+    }
+
+    render() {
+        return (
+            <AuthorizationContext.Provider value={ this.state }>
+                <div style={ { display: 'flex', paddingTop: 100 } }>
+                    <div style={ { boxSizing: 'border-box', background: '', width: '73%', paddingLeft: '6%' } }>
+                        <TwitchPlayer channel="monstercat" />
+                    </div>
+                    <div style={ {
+                        boxSizing: 'border-box',
+                        background: '',
+                        width: '27%',
+                        paddingLeft: '2%',
+                        paddingRight: '6%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                    } }>
+                        <TwitchPlayer channel="rocketblasttv" importance="less" />
+                        <TwitchPlayer channel="wizzlertv" importance="less" />
+                        <TwitchPlayer channel="anything4views" importance="less" />
+                    </div>
+                    {/*<TwitchChat />*/}
+                </div>
+            </AuthorizationContext.Provider>
+        );
+    }
 }
 
 export default Playground;
--- src/components/Twitch/Player.js
+++ src/components/Twitch/Player.js
@@ -1,7 +1,9 @@
 /* global Twitch */
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 import { getId, isLessImportant, useChannel, useVideo, useCollection, setLowestQuality, once } from './Utils';
+
+import AuthorizationContext from './AuthorizationContext.js';
 
 import './Common.css';
 
@@ -11,14 +13,33 @@ class TwitchPlayer extends Component {
     constructor(props) {
         super(props);
 
+        this.ref = createRef();
+
         this.state = {
-            id: getId('player')(props)
+            key: getId('player')(props),
         };
+    }
+
+    componentWillUpdate({ token: { access_token }, channel }) {
+        const { token } = this.props;
+
+        if (!token && access_token && channel) {
+            fetch(`https://api.twitch.tv/helix/users?login=${channel}`, {
+                headers: {
+                    'Authorization': `Bearer ${ access_token }`
+                }
+            })
+                .then(response => response.json())
+                .then(data => this.setState(state => ({
+                    user: data.data[0]
+                })));
+                //.catch(error => console.log(error))
+        }
     }
 
     componentDidMount() {
         const { channel, video, collection } = this.props;
-        const { id } = this.state;
+        const { key } = this.state;
 
         const options = {
             width: '100%',
@@ -29,10 +50,19 @@ class TwitchPlayer extends Component {
             // autoplay: false,
         };
 
-        const player = new Twitch.Player(id, options);
+        const player = new Twitch.Player(key, options);
         
         if (isLessImportant(this.props)) player.setMuted(true);
         // player.setVolume(0.1);
+
+        player.addEventListener(Twitch.Player.READY, () => {
+            // These are big no-no's because everyone cares about security
+            // I can however get the "offline" image from the API and replace the iframe with just that instead
+            //
+            // const node = this.ref.current;
+            // console.log(player._bridge._iframe.contentWindow)
+            // node.querySelector('iframe').contentWindow.document.body.querySelector('.hover-display').style.display//; = 'none';
+        });
 
         player.addEventListener(Twitch.Player.PLAYING, once(() => {
             // setTimeout(() => player.pause(), 5000);
@@ -52,7 +82,7 @@ class TwitchPlayer extends Component {
     }
 
     render() {
-        const { id } = this.state;
+        const { key, user: { display_name: name } = {} } = this.state;
 
         const styles = {
             paddingBottom: `${ (9 / 16 * 100).toFixed(2) }%`,
@@ -60,11 +90,15 @@ class TwitchPlayer extends Component {
 
         return (
             <div style={ { position: 'relative' } }>
-                <div className="root" style={ styles } id={id}/>
-                <div style={ { position: 'absolute', top: '100%', color: 'silver', paddingTop: 5 } }>&nbsp;( &nbsp; ) <strong style={ { color: 'white', fontWeight: 'normal' } }>Channel</strong> Title</div>
+                <div className="root" style={ styles } id={ key } ref={ this.ref }/>
+                <div style={ { position: 'absolute', top: '100%', color: 'gray', paddingTop: 5 } }>&nbsp;( &nbsp; ) <strong style={ { color: 'white', fontWeight: 'normal' } }>{ name }</strong> Title</div>
             </div>
         );
     }
 }
 
-export default TwitchPlayer;
+export default props => (
+    <AuthorizationContext.Consumer>
+        { token => <TwitchPlayer { ...{ ...props, token } }/> }
+    </AuthorizationContext.Consumer>
+);
